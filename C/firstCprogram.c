c504102875535fd7da9e35ab18218f824775e6a7
Avni <avniii1257@gmail.com>
2026-08-11 16:51:41 +0530

    Added Q2

--- C/firstCprogram.c
+++ C/firstCprogram.c
@@ -1,16 +1,16 @@
-//Q1: Write a program to input two numbers and display their sum.
+//Q3: Write a program to calculate the area and perimeter of a rectangle given its length and breadth.
 
 /*
 Sample Test Cases:
 Input 1:
-3 4
+5 10
 Output 1:
-Sum = 7
+Area=50, Perimeter=30
 
 Input 2:
--1 20
+3 7
 Output 2:
-Sum = 19
+Area=21, Perimeter=20
 
 */
 #include<stdio.h>
@@ -21,60 +21,49 @@ int main(){
     int d;
     int e;
     int f;
-    printf("Enter two numbers");
-    scanf("%d%d",&a,&b);
-    c=a+b;
-    printf("Sum:%d\n",c);
-    printf("Enter two numbers");
-    scanf("%d%d",&d,&e);
-    f=d+e;
-    printf("Sum:%d",f);
+    printf("Enter the length of the rectangle");
+    scanf("%d",&a);
+    printf("Enter the breadth of the rectangle");
+    scanf("%d",&b);
+    c=a*b;
+     printf("Area:%d\n",c);
+    d=2*(a+b);
+    printf("Perimeter:%d\n",d);
+    printf("Enter the length of the rectangle");
+    scanf("%d",&e);
+    printf("Enter the breadth of the rectangle");
+    scanf("%d",&f);
+    c=e*f;
+    printf("Area:%d\n",c);
+    d=2*(e+f);
+    printf("Perimeter:%d\n",d);
     return 0;
 }
-//Q2: Write a program to input two numbers and display their sum, difference, product, and quotient.
+\\Q4: Write a program to calculate the area and circumference of a circle given its radius.
 
 /*
 Sample Test Cases:
 Input 1:
-10 2
+7
 Output 1:
-Sum=12, Diff=8, Product=20, Quotient=5
+Area=153.94, Circumference=43.96
 
 Input 2:
-7 3
+3
 Output 2:
-Sum=10, Diff=4, Product=21, Quotient=2
+Area=28.27, Circumference=18.85
 
 */
 #include<stdio.h>
 int main(){
-    int a;
-    int b;
-    int c;
-    int d;
-    int e;
-    int f;
-    int g;
-    int h;
-    printf("Enter two numbers");
-    scanf("%d%d",&a,&b);
-    c=a+b;
-    printf("\nSum:%d",c);
-    d=a-b;
-    printf("\nDifference:%d",d);
-    e=a*b;
-    printf("\nProduct:%d",e);
-    f=a/b;
-    printf("\nQuotient:%d",f);
-    printf("Enter two numbers");
-    scanf("%d%d",&g,&h);
-    c=g+h;
-    printf("\nSum:%d",c);
-    d=g-h;
-    printf("\nDifference:%d",d);
-    e=g*h;
-    printf("\nProduct:%d",e);
-    f=g/h;
-    printf("\nQuotient:%d",f);
+    int r;
+    float a;
+    float b;
+    printf("Enter the radius of the circle");
+    scanf("%d",&r);
+    a=3.14*r*r;
+    printf("Area of the circle:%f",a);
+    b=2*3.14*r;
+    printf("Circumference of the circle:%f",b);
     return 0;
 }
--- C/tempCodeRunnerFile.c
+++ C/tempCodeRunnerFile.c
@@ -1,1 +1,7 @@
-#include<stdio.h>
+//Q3: Write a program to calculate the area and perimeter of a rectangle given its length and breadth.
+
+/*
+Sample Test Cases:
+Input 1:
+5 10
+Output 1:
