ea66807909d208dc276727aef2605daff3f79e68
Avni Singhal <aviii1257@gmail.com>
2026-09-24 23:01:44 +0530

    41.c

---
+++ 41.c
@@ -0,0 +1,66 @@
+//Q85: Reverse a string.
+
+/*
+Sample Test Cases:
+Input 1:
+abcd
+Output 1:
+dcba
+
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+
+        scanf("%s", str);
+
+            int len = strlen(str);
+
+                for (int i = len - 1; i >= 0; i--) {
+                        printf("%c", str[i]);
+                            }
+
+                                return 0;
+                                }
+//Q86: Check if a string is a palindrome.
+
+/*
+Sample Test Cases:
+Input 1:
+madam
+Output 1:
+Palindrome
+
+Input 2:
+hello
+Output 2:
+Not palindrome
+
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+        int i, len, flag = 1;
+
+            scanf("%s", str);
+
+                len = strlen(str);
+
+                    for (i = 0; i < len / 2; i++) {
+                            if (str[i] != str[len - 1 - i]) {
+                                        flag = 0;
+                                                    break;
+                                                            }
+                                                                }
+
+                                                                    if (flag == 1)
+                                                                            printf("Palindrome");
+                                                                                else
+                                                                                        printf("Not palindrome");
+
+                                                                                            return 0;
+                                                                                            }
